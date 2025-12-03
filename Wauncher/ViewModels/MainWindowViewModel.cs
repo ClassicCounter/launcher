d86ade2cd6c689ec47501437948db19c10c79ad9
Nikolaus <github@koolych.ru>
2025-12-03 16:52:18 +1000

    New CommunityToolkit AvaloniaUI App Init

---
+++ Wauncher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Wauncher.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Wauncher/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Wauncher.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
