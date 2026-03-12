4db59205a4a9640d6582c24740f34aafde3fdb55
Ways <109334069+ways15xx@users.noreply.github.com>
2026-03-12 17:22:26 -0400

    Add patch note date headers

--- Wauncher/ViewModels/PatchNoteItem.cs
+++ Wauncher/ViewModels/PatchNoteItem.cs
@@ -4,6 +4,7 @@ namespace Wauncher.ViewModels
     {
         public string Text { get; set; } = string.Empty;
         public bool IsMajorHeader { get; set; }
+        public bool IsDateHeader { get; set; }
         public bool IsHeader { get; set; }
         public bool IsBullet { get; set; }
     }
